c2cec6f533a268c5d27c99dff8b754cf1aab78fd
yashijain1234 <73181774+yashijain1234@users.noreply.github.com>
2022-02-25 18:56:40 +0530

    Delete calculator directory

--- calculator/backend/InvestWell/routes/index.js
+++
@@ -1,7 +0,0 @@
-const { sipStepUpCalculator } = require("../controller/index")
-const express = require("express")
-const router = express.Router()
-
-router.post("/sipStepUpCalculator", sipStepUpCalculator)
-
-module.exports =  router
